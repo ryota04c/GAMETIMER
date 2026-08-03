096f76f608e73e35dafb53f9d77e3875a9869805
ryota04c <135027002+ryota04c@users.noreply.github.com>
2026-08-03 16:41:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,212 @@
-console.log("layout test");
+const board =
+    document.getElementById("board");
+
+
+const playerCount =
+    document.getElementById("playerCount");
+
+
+
+
+
+// 座席配置
+
+const seatLayouts = {
+
+
+    2:[
+        "top",
+        "bottom"
+    ],
+
+
+    3:[
+        "top",
+        "right",
+        "left"
+    ],
+
+
+    4:[
+        "top",
+        "right",
+        "bottom",
+        "left"
+    ],
+
+
+    5:[
+        "top",
+        "topRight",
+        "bottomRight",
+        "bottomLeft",
+        "left"
+    ],
+
+
+    6:[
+        "top",
+        "topRight",
+        "right",
+        "bottomRight",
+        "bottomLeft",
+        "left"
+    ],
+
+
+    7:[
+        "top",
+        "topRight",
+        "right",
+        "bottomRight",
+        "bottom",
+        "bottomLeft",
+        "left"
+    ],
+
+
+    8:[
+        "top",
+        "topRight",
+        "right",
+        "bottomRight",
+        "bottom",
+        "bottomLeft",
+        "left",
+        "topLeft"
+    ]
+
+};
+
+
+
+
+
+// 回転
+
+const rotations = {
+
+
+    top:180,
+
+
+    topRight:-135,
+
+
+    right:-90,
+
+
+    bottomRight:-45,
+
+
+    bottom:0,
+
+
+    bottomLeft:45,
+
+
+    left:90,
+
+
+    topLeft:135
+
+
+};
+
+
+
+
+
+
+function createSeats(count){
+
+
+    board.innerHTML="";
+
+
+
+    let seats =
+        seatLayouts[count];
+
+
+
+    seats.forEach(
+        (seat,index)=>{
+
+
+            let div =
+            document.createElement("div");
+
+
+
+            div.className =
+                "seat seat-"+seat;
+
+
+
+            div.style.transform =
+                `
+                rotate(${rotations[seat]}deg)
+                `;
+
+
+
+            div.innerHTML = `
+
+
+                <div class="name">
+
+                    Player${index+1}
+
+                </div>
+
+
+                <div class="time">
+
+                    30.000
+
+                </div>
+
+
+                <button>
+
+                    終了
+
+                </button>
+
+
+            `;
+
+
+
+            board.appendChild(div);
+
+
+        }
+
+    );
+
+
+}
+
+
+
+
+
+playerCount.onchange=()=>{
+
+
+    createSeats(
+        Number(playerCount.value)
+    );
+
+
+};
+
+
+
+
+
+// 初期表示
+
+createSeats(4);
